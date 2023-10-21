df2d1471dc6060ded4bd9b1993c76e59752c3fb5
shashank52i <97112921+shashank52i@users.noreply.github.com>
2023-10-20 20:28:14 -0400

    Update settings.py

--- ailabcms/ailabcms/ailab/settings.py
+++ ailabcms/ailabcms/ailab/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '$%fge_21^x*n&wc*k#^azid5fu7y!df8_z&3&514sb^k(hy9%='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
