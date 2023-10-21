4045ede13687d4040b241bdeb130f2c4e2029845
shashank52i <97112921+shashank52i@users.noreply.github.com>
2023-10-21 17:46:49 -0400

    Update settings.py

--- ailabcms/ailab/settings.py
+++ ailabcms/ailab/settings.py
@@ -208,7 +208,8 @@ CMS_TEMPLATES = (
     ## Customize this
     ('fullwidth.html', 'Fullwidth'),
     ('sidebar_left.html', 'Sidebar Left'),
-    ('sidebar_right.html', 'Sidebar Right')
+    ('sidebar_right.html', 'Sidebar Right'),
+    ('indec.html', 'indec'),
 
 )
 
