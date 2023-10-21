27a2020f794f72f775e0c2e52108bc233c706c4e
shashank52i <97112921+shashank52i@users.noreply.github.com>
2023-10-21 17:33:13 -0400

    Update settings.py

--- ailabcms/ailabcms/ailab/settings.py
+++ ailabcms/ailabcms/ailab/settings.py
@@ -208,7 +208,7 @@ CMS_TEMPLATES = (
     ('fullwidth.html', 'Fullwidth'),
     ('sidebar_left.html', 'Sidebar Left'),
     ('sidebar_right.html', 'Sidebar Right'),
-    ('About.html', 'About'),
+    ('indec.html', 'indec'),
 )
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
