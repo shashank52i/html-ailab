1e05051594c90b6542b465bcca528378767da86a
shashank52i <shashankgadhe1999@gmail.com>
2023-10-22 14:06:25 -0400

    .

--- ailabcms/ailabcms/ailab/settings.py
+++ ailabcms/ailabcms/ailab/settings.py
@@ -97,9 +97,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(DATA_DIR, 'media')
 STATIC_ROOT = os.path.join(DATA_DIR, 'static')
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'ailab', 'static'),
-)
+
 SITE_ID = 1
 
 
