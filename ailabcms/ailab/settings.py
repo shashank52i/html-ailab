4dd448587f0424e152aaac94ea2b2101cfe59727
shashank52i <shashankgadhe1999@gmail.com>
2023-10-22 13:35:35 -0400

    .

--- ailabcms/ailab/settings.py
+++ ailabcms/ailab/settings.py
@@ -20,6 +20,9 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Add the DATA_DIR definition below
+DATA_DIR = os.path.join(BASE_DIR, 'ailab', 'templates')
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
