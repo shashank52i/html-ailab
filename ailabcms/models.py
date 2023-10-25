cba2a44f07984318594df8acaebb192af9a6aa01
shashank52i <shashankgadhe1999@gmail.com>
2023-10-25 13:48:42 -0400

    .

--- ailabcms/models.py
+++ ailabcms/models.py
@@ -4,4 +4,4 @@ from django.db import models
 
 class ailab(CMSPlugin):
     title = models.CharField(max_length=50, default='Title')
-    body = models.CharField(max_length=1000,default='Content')
+    body = models.CharField(max_length=5000,default='Content')
